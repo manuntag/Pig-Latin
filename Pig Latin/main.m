1186c8a018987709ca47b38daab60ad7b4444549
manuntag <manuntag@ualberta.ca>
2015-01-16 17:36:06 -0800

    Initial Commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by David Manuntag on 2015-01-16.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
